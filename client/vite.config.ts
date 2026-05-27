9f9e7d11fc111de7a29375864cc9b32bdcadd0eb
moshfekuzzahan-shefa <shefabsmrstucse@gmail.com>
2026-05-27 15:53:36 +0600

    feat: configure Vite server with proxy settings for API integration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     proxy: {
       // Whenever React sees a fetch starting with '/api', it secretly forwards it to port 5000!
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
       }
